a7c6a9efd59e2b861b8213eb65f0282d6ce2255f
AidenLeeeee <hoo8799@naver.com>
2022-10-08 16:02:00 +0900

    Add Chat Service Logic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,12 +9,41 @@ const formElement = getElementById('chat_form');
 
 //* global socket handler
 socket.on('user_connected', (userName) => {
-  console.log(`${userName} connected!`);
+  drawNewchat(`${userName} connected!`);
 });
+
+socket.on('new_chat', (data) => {
+  const { chat, userName } = data;
+  drawNewchat(`${userName}: ${chat}`);
+});
+
+//* event callback functions
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  console.log(inputValue);
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    // Draw on Screen
+    drawNewchat(`Me: ${inputValue}`);
+    event.target.elements[0].value = '';
+  }
+};
 
 //* draw functions
 const drawHelloStranger = (userName) => {
   helloStrangerElement.innerHTML = `Hello ${userName} Stranger :)`;
+};
+
+const drawNewchat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `
+    <div>
+      ${message}
+    </div>
+  `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
 };
 
 function helloUser() {
@@ -26,6 +55,8 @@ function helloUser() {
 
 function init() {
   helloUser();
+  // Event Connect
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
